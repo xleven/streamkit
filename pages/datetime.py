d0ec2055e90ec2761d3428c066a87fa7b7aabf96
xleven <xleven@outlook.com>
2022-10-25 10:48:16 +0800

    fix text_input bug

--- pages/datetime.py
+++ pages/datetime.py
@@ -2,6 +2,8 @@ import streamlit as st
 from datetime import datetime
 
 
+st.set_page_config(page_title="StreamKit · web toolbox")
+
 st.header("Datatime toolbox")
 
 st.subheader("Delta between 2 dates")
@@ -12,21 +14,22 @@ with st.form("datepicker"):
         st.write(da-db)
 
 st.subheader("Timestamp Converter")
-tab1, tab2 = st.tabs(["timestamp to datetime", "datetime to timestamp"])
-with tab1:
-    ts = st.text_input("timestamp in senconds", value=datetime.now().timestamp())
-    try:
-        ts = float(ts)
-    except:
-        st.error("Error: please check your input")
-    else:
-        dt = datetime.fromtimestamp(ts)
-        st.write(dt)
-with tab2:
-    with st.form("datetiem"):
-        date = st.date_input("date")
-        time = st.time_input("time")
-        if st.form_submit_button("Convert"):
-            dt = datetime.combine(date, time)
-            ts = datetime.timestamp(dt)
-            st.write(ts)
+with st.form("timestamp"):
+    st.write("timestamp to datetime")
+    ts1 = st.text_input("timestamp in senconds", value=1666666666)
+    if st.form_submit_button("Convert"):
+        try:
+            ts1 = int(float(ts1))
+        except:
+            st.error("Error: please check your input")
+        else:
+            dt1 = datetime.fromtimestamp(ts1)
+            st.write(dt1)
+with st.form("datetiem"):
+    st.write("datetime to timestamp")
+    date = st.date_input("date")
+    time = st.time_input("time")
+    if st.form_submit_button("Convert"):
+        dt2 = datetime.combine(date, time)
+        ts2 = datetime.timestamp(dt2)
+        st.write(ts2)
